35ed91493ede5cccd8caf776b38be68d7b78a95d
iClon <iClon@Mac-Pro-de-iClon.local>
2021-02-08 18:03:03 +0100

    Primera Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RelojComponent } from './reloj/reloj.component';
+import { JugadorComponent } from './jugador/jugador.component';
+import { VerificadorComponent } from './verificador/verificador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RelojComponent,
+    JugadorComponent,
+    VerificadorComponent
   ],
   imports: [
     BrowserModule
